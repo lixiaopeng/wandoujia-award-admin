02de0bbe616194c3f0b6b3d90a35d320e6f873c5
J.Y Han <ihanjiyun@gmail.com>
2014-05-27 20:48:29 +0800

    add Pen editor support

--- app/src/javascripts/award.js
+++ app/src/javascripts/award.js
@@ -1,10 +1,10 @@
-/*global $, Modernizr, require, _, __dirname, suggestion, module*/
+/*global $, Modernizr, require, _, __dirname, suggestion, Pen, module*/
 
 /*
 * @Author: hanjiyun
 * @Date:   2014-05-22 18:29:11
 * @Last Modified by:   hanjiyun
-* @Last Modified time: 2014-05-25 11:42:09
+* @Last Modified time: 2014-05-27 20:46:23
 */
 
 $(function () {
@@ -14,8 +14,15 @@ $(function () {
     var appSearchListTpl = $('#app-search-list-tpl');
     var editorHeaderTpl = $('#editor-header-tpl');
     var editorAppBoxTpl = $('#editor-app-box-tpl');
-    var editor = $('#editor');
+    var editorWrap = $('#editor');
     var isEditing = false;
+    var editorOptions = {
+        editor: document.getElementById('content'), // {DOM Element} [required]
+        class: 'pen', // {String} class of the editor,
+        debug: true, // {Boolean} false by default
+        textarea: '<textarea name="content"></textarea>', // fallback for old browsers
+        list: ['bold', 'italic', 'underline'] // editor menu list
+    };
 
     var renderAppSearchListTpl = function (appList) {
         return (_.template(appSearchListTpl.html()))({appList: appList});
@@ -70,9 +77,9 @@ $(function () {
                     // 得到用户选定的app 信息，插入到模板中
                     var appData = data.appList[0];
 
-                    console.log('data', data);
+                    console.log('所有data', data);
 
-                    console.log('appData', appData);
+                    console.log('单独某个appData', appData);
 
                     if (!appData) {
                         showError();
@@ -81,16 +88,31 @@ $(function () {
 
                     // 输出编辑页面 头部
                     var editorHeader = renderEditorHeaderTpl(appData);
-                    editor.prepend(editorHeader);
+                    editorWrap.prepend(editorHeader);
 
                     // 输出编辑页面 app 块
                     var appBox = renderEditorAppBoxTpl(appData);
+
+                    var oneScreenArray = appData.screenshots.small[0].split('_');
+                    var screenWidth = oneScreenArray[1];
+                    var screenHeight = oneScreenArray[2].split('.')[0];
+
+                    console.log('screenWidth 宽', screenWidth, 'screenHeight 高', screenHeight);
+
+
+
                     $('#app-box').html(appBox);
+
+                    $('.screenshot-list').width(screenWidth * appData.screenshots.small.length + ((appData.screenshots.small.length - 1) * 5));
 
                     // 隐藏搜索层
                     searchAppOverlay.fadeOut(function () {
                         $(this).remove();
                     });
+
+
+                    // 初始化编辑器
+                    var myPen = new Pen(editorOptions);
 
                     isEditing = true;
                 }
@@ -99,13 +121,15 @@ $(function () {
     }
 
     function showError() {
+        // TODO
         alert('出错了');
     }
 
-    // 页面刷新
+    // 页面刷新，如果是正在编辑状态，则弹出提示
     window.onbeforeunload = function (e) {
-        if (isEditing) {
-            return '文章还没有保存';
-        }
+        // TODO
+        // if (isEditing) {
+        //     return '文章还没有保存';
+        // }
     };
 });
